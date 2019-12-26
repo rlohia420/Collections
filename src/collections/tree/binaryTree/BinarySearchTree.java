0b7c6370f7282e282c70813b46e4cb087d016559
root <neha.lohia@naukri.com>
2019-12-26 09:47:29 +0530

    Changes to add the bnarysearchTree

--- src/collections/tree/binaryTree/BinarySearchTree.java
+++ src/collections/tree/binaryTree/BinarySearchTree.java
@@ -2,16 +2,34 @@ package collections.tree.binaryTree;
 
 public class BinarySearchTree {
 
-	public boolean isBst(Node node){
-		if(node==null)
+	public boolean isBst(Node node) {
+		if (node == null)
 			return true;
-		if(node.left!=null && maxValue(node.left) > node.data)
+		if (node.left != null && maxValue(node.left) > node.data)
 			return false;
-		if(node.right!=null && minValue(node.right) < node.data)
+		if (node.right != null && minValue(node.right) < node.data)
 			return false;
-		if(!isBst(node.left) || !isBst(node.right)) {
+		if (!isBst(node.left) || !isBst(node.right)) {
 			return false;
 		}
 		return true;
+	}
+
+	public int maxValue(Node node) {
+		Node current = node;
+		/* loop down to find the leftmost leaf */
+		while (current.right != null) {
+			current = current.right;
+		}
+		return (current.data);
+	}
+
+	public int minValue(Node node) {
+		Node current = node;
+		/* loop down to find the leftmost leaf */
+		while (current.left != null) {
+			current = current.left;
+		}
+		return (current.data);
 	}
 }
